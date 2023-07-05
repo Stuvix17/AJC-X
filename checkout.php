73dce19ae6c98a689960e479f33be155cebd42c7
Stuvix17 <129030120+Stuvix17@users.noreply.github.com>
2023-07-05 13:34:06 +0530

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,148 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+if(isset($_POST['order_btn'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $number = $_POST['number'];
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $method = mysqli_real_escape_string($conn, $_POST['method']);
+   $address = mysqli_real_escape_string($conn, $_POST['flat']);
+   $placed_on = date('d-M-Y');
+
+   $cart_total = 0;
+   $cart_products[] = '';
+
+   $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+   if(mysqli_num_rows($cart_query) > 0){
+      while($cart_item = mysqli_fetch_assoc($cart_query)){
+         $cart_products[] = $cart_item['name'].' ('.$cart_item['quantity'].') ';
+         $sub_total = ($cart_item['price'] * $cart_item['quantity']);
+         $cart_total += $sub_total;
+      }
+   }
+
+   $total_products = implode(', ',$cart_products);
+
+   $order_query = mysqli_query($conn, "SELECT * FROM `orders` WHERE name = '$name' AND number = '$number' AND email = '$email' AND method = '$method' AND address = '$address' AND total_products = '$total_products' AND total_price = '$cart_total'") or die('query failed');
+
+   if($cart_total == 0){
+      $message[] = 'your cart is empty';
+   }else{
+      if(mysqli_num_rows($order_query) > 0){
+         $message[] = 'order already placed!'; 
+      }else{
+         mysqli_query($conn, "INSERT INTO `orders`(user_id, name, number, email, method, address, total_products, total_price, placed_on) VALUES('$user_id', '$name', '$number', '$email', '$method', '$address', '$total_products', '$cart_total', '$placed_on')") or die('query failed');
+         $message[] = 'order placed successfully!';
+         mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+      }
+   }
+   
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Checkout | AJC-X</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>checkout</h3>
+   <p> <a href="home.php">Home</a> / Checkout </p>
+</div>
+
+<section class="display-order">
+
+   <?php  
+      $grand_total = 0;
+      $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+      if(mysqli_num_rows($select_cart) > 0){
+         while($fetch_cart = mysqli_fetch_assoc($select_cart)){
+            $total_price = ($fetch_cart['price'] * $fetch_cart['quantity']);
+            $grand_total += $total_price;
+   ?>
+   <p> <?php echo $fetch_cart['name']; ?> <span>(<?php echo '₹'.$fetch_cart['price'].'/-'.' x '. $fetch_cart['quantity']; ?>)</span> </p>
+   <?php
+      }
+   }else{
+      echo '<p class="empty">Your cart is empty</p>';
+   }
+   ?>
+   <div class="grand-total"> Grand total : <span>₹<?php echo $grand_total; ?>/-</span> </div>
+
+</section>
+
+<section class="checkout">
+
+   <form action="" method="post">
+      <h3>Place your order</h3>
+      <div class="flex">
+         <div class="inputBox">
+            <span>Your name :</span>
+            <input type="text" name="name" required placeholder="Enter your name">
+         </div>
+         <div class="inputBox">
+            <span>Your number :</span>
+            <input type="number" name="number" required placeholder="Enter your number">
+         </div>
+         <div class="inputBox">
+            <span>Your email :</span>
+            <input type="email" name="email" required placeholder="Enter your email">
+         </div>
+         <div class="inputBox">
+            <span>Payment method :</span>
+            <select name="method">
+               <option value="cash on delivery">Cash on delivery</option>
+               <option value="UPI PAYMENT ONLINE">Upi Payment Online (xyz@okaxis)</option>
+               <option value="UPI PAYMENT ON DELIVERY">Upi Payment on delivery</option>
+            </select>
+         </div>
+         <div class="inputBox">
+            <span>Select your preferable time for pickup :</span>
+            <input type="time" name="flat" required placeholder="FROM 10 AM TO 6 PM">
+         </div>
+      </div>
+      <input type="submit" value="order now" class="btn" name="order_btn">
+   </form>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,32 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91 62823 12175 </p>
+         <p> <i class="fas fa-envelope"></i> ajcxit@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> IT Department,<br>   Amal Jyothi College of Engineering,<br> Kottayam. </p>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; Copyright  @ <?php echo date('Y'); ?> by <span>TEAM AJC-X</span> </p>
+
+</section>
